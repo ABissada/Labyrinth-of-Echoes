ab56bc4faf8cbd6ddf5c2590b73f9249efd3ce92
Andrew Bissada <andrewjamesbissada@gmail.com>
2024-02-11 16:41:59 -0500

    Finished player movement script

--- Labyrinth of Echoes/Assets/movement.cs
+++ Labyrinth of Echoes/Assets/movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,12 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
 
-    public float moveSpeed = 3f;
+    public float moveSpeed = 5f;
     public Transform movePoint;
 
     public LayerMask whatStopsMovement;
+
+    public SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +24,27 @@ public class movement : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= .03f) {
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.5f) {
 
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            if (Input.GetAxisRaw("Horizontal") == -1f) {
+                sr.flipX = true;
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
 
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            if (Input.GetAxisRaw("Horizontal") == 1f) {
+                sr.flipX = false;
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+
+            if (Math.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)) {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
         }
     }
 }
