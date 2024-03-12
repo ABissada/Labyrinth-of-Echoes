4824fe0ae5c3d22188206ef98ad41599415ef5c9
Andrew Bissada <andrewjamesbissada@gmail.com>
2024-03-12 18:19:59 -0400

    fixed bugs in player movement

--- Labyrinth of Echoes/Assets/Scripts/player_movement.cs
+++ Labyrinth of Echoes/Assets/Scripts/player_movement.cs
@@ -7,42 +7,50 @@ public class player_movement : MonoBehaviour
 {
 
     public float moveSpeed = 3f;
-    public Transform movePoint;
+    public Transform playerMovePoint;
 
     public LayerMask whatStopsMovement;
 
     public SpriteRenderer sr;
 
+    public int moveCount = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        movePoint.parent = null;
+        playerMovePoint.parent = null;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, playerMovePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) == 0f) {
+        if (Vector3.Distance(transform.position, playerMovePoint.position) == 0f) {
 
             if (Input.GetAxisRaw("Horizontal") == -1f) {
+                moveCount += 1;
                 sr.flipX = true;
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                if(!Physics2D.OverlapCircle(playerMovePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
+                    playerMovePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
 
             else if (Input.GetAxisRaw("Horizontal") == 1f) {
+                moveCount += 1;
                 sr.flipX = false;
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                if(!Physics2D.OverlapCircle(playerMovePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
+                    playerMovePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
 
             else if (Math.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)) {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                moveCount += 1;
+                if(!Physics2D.OverlapCircle(playerMovePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)) {
+                    playerMovePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
             }
         }
