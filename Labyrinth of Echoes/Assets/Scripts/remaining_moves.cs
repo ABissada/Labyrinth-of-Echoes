55dbdef7371835e412bd1829b348ddbe2047de98
Roman Rodchenkov <rodcheroma@gmail.com>
2024-03-14 15:51:41 -0400

    Made working in-game UI
    - move counter decrements
    - reset level
    - menu with volume, main, and map buttons

---
+++ Labyrinth of Echoes/Assets/Scripts/remaining_moves.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class remaining_moves : MonoBehaviour
+{
+    public int max_moves;
+
+    public player_movement_refactored P;
+
+    public bool outOfMoves;
+
+    public TextMeshProUGUI moves_remaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outOfMoves = false;
+        P = FindObjectOfType<player_movement_refactored>();
+        moves_remaining.SetText(max_moves.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moves_remaining.SetText((max_moves - P.moveCount).ToString());
+        if (max_moves - P.moveCount == 0)
+        {
+            outOfMoves = true;
+        }
+        else
+        {
+            outOfMoves = false;
+        }
+    }
+}
