5525ebee4db6ca2483945972b0b4917146dbfca1
EvasiveJosh <joshua.yarussky@gmail.com>
2024-03-14 16:15:04 -0400

    finished level 2

--- Labyrinth of Echoes/Assets/Scripts/PopUp.cs
+++ Labyrinth of Echoes/Assets/Scripts/PopUp.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 
 public class PopUp : MonoBehaviour
 {
+    public bool movement = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,10 @@ public class PopUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D))
+        {
+            movement = true;
+            gameObject.SetActive(!movement);
+        }
     }
 }
