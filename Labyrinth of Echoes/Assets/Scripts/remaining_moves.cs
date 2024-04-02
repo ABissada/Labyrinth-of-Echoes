e109d4683cba8c5bba19d9dc586f4db28ecab941
Roman Rodchenkov <rodcheroma@gmail.com>
2024-04-02 18:55:40 -0400

    Changes to movecount and some level progress

--- Labyrinth of Echoes/Assets/Scripts/remaining_moves.cs
+++ Labyrinth of Echoes/Assets/Scripts/remaining_moves.cs
@@ -24,8 +24,8 @@ public class remaining_moves : MonoBehaviour
     {
         for (int i = 0; i<P.Length; i++)
         {
-            moves_remaining.SetText((config.maxMoves - P[i].moveCount).ToString());
-            if (config.maxMoves - P[i].moveCount <= 0)
+            moves_remaining.SetText((config.maxMoves - P[0].moveCount).ToString());
+            if (config.maxMoves - P[0].moveCount <= 0)
             {
                 P[i].counter = 0;
             }
