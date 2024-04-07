116e4fce22080e9522aa20367007915bde3bbc99
EvasiveJosh <joshua.yarussky@gmail.com>
2024-04-06 22:55:04 -0400

    tutorial done

--- Labyrinth of Echoes/Assets/Scripts/PopUp.cs
+++ Labyrinth of Echoes/Assets/Scripts/PopUp.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using UnityEditor.VersionControl;
 
 public class PopUp : MonoBehaviour
 {
-    public bool movement = false;
+ 
     // Start is called before the first frame update
+
     void Start()
     {
         
@@ -15,10 +18,6 @@ public class PopUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D))
-        {
-            movement = true;
-            gameObject.SetActive(!movement);
-        }
+
     }
 }
